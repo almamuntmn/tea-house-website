93c62de070e03c75239fc2fab383749fb237cf03
almamuntmn <almamuncool88@gmail.com>
2025-06-24 10:27:20 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // or adjust this depending on your files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
